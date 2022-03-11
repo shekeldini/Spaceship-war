8976e23e48e16c210e6e132845ee197c5585351d
Исаян Денис Валерьевич <idv@akiac.ru>
2022-03-11 13:42:34 +0700

    add .gitignore and requirements

--- classes/bullets/enemy_bullet.py
+++ classes/bullets/enemy_bullet.py
@@ -4,6 +4,6 @@ from classes.bullets.bullet import Bullet
 class EnemyBullet(Bullet):
     def __init__(self, image, pos, rotate_coef, size, damage):
         self._size = size
-        self._speed = 5
+        self._speed = 10
         self._damage = damage
         super().__init__(image, pos, rotate_coef, self._size, self._speed, self._damage)
